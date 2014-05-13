dbece5a16167317935a86df8eedc288ef086b579
André Santos <andreccdr@gmail.com>
2014-05-13 20:26:49 +0100

    Many-To-Many Relation with inheritance in Expense-ExpenseType

--- grails-app/domain/com/billmate/DefaultExpenseType.groovy
+++ grails-app/domain/com/billmate/DefaultExpenseType.groovy
@@ -1,8 +1,6 @@
 package com.billmate
 
 class DefaultExpenseType extends ExpenseType{
-    static belongsTo = CircleType
-    static hasMany = [circleTypes: CircleType]
 
     static constraints = {
     }
--- grails-app/domain/com/billmate/ExpenseType.groovy
+++ grails-app/domain/com/billmate/ExpenseType.groovy
@@ -3,15 +3,15 @@ package com.billmate
 import com.lucastex.grails.fileuploader.UFile
 
 abstract class ExpenseType {
-    static belongsTo = [Circle, Expense]
-    static hasMany = [circles: Circle, expenses: Expense]
+    static belongsTo = [Circle, Expense, CircleType]
+    static hasMany = [circles: Circle, expenses: Expense, circleTypes: CircleType]
 
     String name
     UFile icon
 
     static constraints = {
         name blank: false, nullable: false
-        icon nullable: false
+        icon nullable: true
     }
 
     String toString(){
