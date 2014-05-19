5dbc0af11fc6bbcec1be77e7ad9582c536c8b72f
pmcleite <pmcleite@gmail.com>
2014-05-19 01:31:05 +0100

    add dashboard. add js. add trigger to close menu. add text-wrap-break to user name in menu

---
+++ assets/javascripts/billmate.js
@@ -0,0 +1,44 @@
+$( "#close-fast-notif" ).click(function() {
+  $( "#dashboard-recent" ).hide( "slow" );
+});
+
+$(".notifs-right .lab").click(function(){
+
+	var $act = $(this).parents("div[class^=comment-recent]").hide("slow");
+	var $bool = $(this).parents("div[class^=comment-recent]").hasClass("first");
+	var $count = $(this).parents("div[class^=comment-recent]").siblings().filter(function() {
+			return $(this).css('display') != 'none';
+		}).length;
+	var $text = $(this).parents("div[class^=comment-text]").siblings().text();
+	
+	if($bool){
+		$(this).parents("div[class^=comment-recent]").siblings().filter(function() {
+			return $(this).css('display') != 'none';
+		}).first().addClass("first");
+		$(this).parents("div[class^=comment-recent]").siblings().filter(function() {
+			return $(this).css('display') == 'none';
+		}).removeClass("comment comment-recent");
+		$(this).parents("div[class^=comment-recent]").removeClass("comment comment-recent");
+	}
+
+	$.growl.notice({ title: $text, message: "A despesa já foi recebida" });
+
+	if($count == 0)
+	{
+		$(this).parents("div[class^=panel]").hide("slow");
+		$.growl.notice({ title: "Aviso", message: "Todas as despesas foram recebidas" });
+	}
+
+});
+
+$("#closeMenu").click(function(){
+	$("#main-menu-toggle").trigger("click");
+});
+
+$("#nameMenu").click(function(){
+	if($(this).hasClass("text-wrap-break")){
+		$(this).removeClass("text-wrap-break");
+	}else{
+		$(this).addClass("text-wrap-break");
+	}
+});
