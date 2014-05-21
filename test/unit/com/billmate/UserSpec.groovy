f7c5f6cba8d373baf6dfef760dc31a54583f2309
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 06:02:44 +0100

    test user all constraints

--- test/unit/com/billmate/UserSpec.groovy
+++ test/unit/com/billmate/UserSpec.groovy
@@ -1,40 +1,35 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(User)
-class UserSpec extends Specification {
+class UserSpec extends ConstraintUnitSpec {
 
-    def myUser
-
-    @Before
     def setup() {
-        myUser = new User(name: "John Doe", email: "johndoe@mail.com",createdAt: new Date())
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(User, [new User(email: "johndoe@mail.com")])
     }
 
-    @After
-    def cleanup() {
+    @Unroll("test user all constraints #field is #error")
+    def "test user all constraints"() {
+        when:
+        def obj = new User("$field": val)
+
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field        | val
+        'nullable'             | 'email'      | null
+        'email'                | 'email'      | "john"
+        'unique'               | 'email'      | "johndoe@mail.com"
+        'nullable'             | 'createdAt'  | null
+        'min'                  | 'createdAt'  | new Date() - 1
+
     }
 
-    @Test
-    void "Test that email is an valid format"() {
-
-        when: 'mail format is valid'
-        then: 'validation should pass'
-        myUser.validate(['email'])
-
-        when: 'mail format is invalid'
-        myUser.setEmail("notanemail")
-
-        then: 'validation should fail'
-        !myUser.validate(['email'])
-
-    }
 }
