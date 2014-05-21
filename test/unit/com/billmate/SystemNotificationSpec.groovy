7f97ae38d1e8f8ee07fbfef673b16d14fb44c096
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 10:14:41 +0100

    test systemNotification all constraints

--- test/unit/com/billmate/SystemNotificationSpec.groovy
+++ test/unit/com/billmate/SystemNotificationSpec.groovy
@@ -1,20 +1,32 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(SystemNotification)
-class SystemNotificationSpec extends Specification {
+class SystemNotificationSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(SystemNotification, [new SystemNotification()])
     }
 
-    def cleanup() {
-    }
+    @Unroll("test systemNotification all constraints #field is #error")
+    def "test systemNotification all constraints"() {
+        when:
+        def obj = new SystemNotification("$field": val)
 
-    void "test something"() {
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field            | val
+        'nullable'             | 'action'         | null
+        'nullable'             | 'registeredUser' | null
+        'nullable'             | 'notification'   | null
+        'nullable'             | 'read'           | null
     }
 }
