62e8d6365c562b0823991e7f4bf068263eceddaf
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 06:18:09 +0100

    test actiontype all constraints

--- test/unit/com/billmate/ActionTypeSpec.groovy
+++ test/unit/com/billmate/ActionTypeSpec.groovy
@@ -1,20 +1,30 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(ActionType)
-class ActionTypeSpec extends Specification {
+class ActionTypeSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(ActionType, [new ActionType()])
     }
 
-    def cleanup() {
-    }
+    @Unroll("test actionType all constraints #field is #error")
+    def "test actionType all constraints"() {
+        when:
+        def obj = new ActionType("$field": val)
 
-    void "test something"() {
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field        | val
+        'blank'                | 'type'       | ''
+        'nullable'             | 'type'       | null
     }
 }
