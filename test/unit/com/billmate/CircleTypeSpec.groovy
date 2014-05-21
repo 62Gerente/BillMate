dd538e24510bacd5923e267f60383e70f8faff00
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 06:21:08 +0100

    test circleType all constraints

--- test/unit/com/billmate/CircleTypeSpec.groovy
+++ test/unit/com/billmate/CircleTypeSpec.groovy
@@ -1,20 +1,31 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(CircleType)
-class CircleTypeSpec extends Specification {
+class CircleTypeSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(CircleType, [new CircleType()])
     }
 
-    def cleanup() {
-    }
+    @Unroll("test circleType all constraints #field is #error")
+    def "test circleType all constraints"() {
+        when:
+        def obj = new CircleType("$field": val)
 
-    void "test something"() {
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field        | val
+        'nullable'             | 'identifier' | null
+        'nullable'             | 'name'       | null
+        'blank'                | 'name'       | ''
     }
 }
