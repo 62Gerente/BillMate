f760a3d8c9f61e39eb36f42e0b2c80652fab4d18
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 09:26:01 +0100

    test house  all constraints

--- test/unit/com/billmate/HouseSpec.groovy
+++ test/unit/com/billmate/HouseSpec.groovy
@@ -1,20 +1,29 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(House)
-class HouseSpec extends Specification {
+class HouseSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(House, [new House()])
     }
 
-    def cleanup() {
-    }
+    @Unroll("test house all constraints #field is #error")
+    def "test house all constraints"() {
+        when:
+        def obj = new House("$field": val)
 
-    void "test something"() {
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field          | val
+        'nullable'             | 'circle'         | null
     }
 }
