1726c4f1fd47ac57836967f04ade71d5aad3d310
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 05:54:16 +0100

    test circle all constraints

--- test/unit/com/billmate/CircleSpec.groovy
+++ test/unit/com/billmate/CircleSpec.groovy
@@ -1,20 +1,32 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Circle)
-class CircleSpec extends Specification {
+class CircleSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(Circle, [new Circle()])
     }
 
-    def cleanup() {
+    @Unroll("test circle all constraints #field is #error")
+    def "test circle all constraints"() {
+        when:
+        def obj = new Circle("$field": val)
+
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field        | val
+        'nullable'             | 'name'       | null
+        'blank'                | 'name'       | ''
+        'nullable'             | 'createdAt'  | null
     }
 
-    void "test something"() {
-    }
 }
