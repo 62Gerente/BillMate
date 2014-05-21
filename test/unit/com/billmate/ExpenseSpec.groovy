cab6a8b841f4197bdfd61e20396477aa14f5c09d
ricardobranco <28.ricardobranco@gmail.com>
2014-05-21 08:16:41 +0100

    test expense  all constraints

--- test/unit/com/billmate/ExpenseSpec.groovy
+++ test/unit/com/billmate/ExpenseSpec.groovy
@@ -1,20 +1,36 @@
 package com.billmate
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Expense)
-class ExpenseSpec extends Specification {
+class ExpenseSpec extends ConstraintUnitSpec {
 
     def setup() {
+        //mock a person with some data (put unique violations in here so they can be tested, the others aren't needed)
+        mockForConstraintsTests(Expense, [new Expense()])
     }
 
-    def cleanup() {
-    }
+    @Unroll("test expense all constraints #field is #error")
+    def "test expense all constraints"() {
+        when:
+        def obj = new Expense("$field": val)
 
-    void "test something"() {
+        then:
+        validateConstraints(obj, field, error)
+
+        where:
+        error                  | field          | val
+        'nullable'             | 'responsible'  | null
+        'nullable'             | 'title'        | null
+        'blank'                | 'title'        | ""
+        'maxSize'              | 'description'  | 'a'*2001
+        'min'                  | 'value'        | -1
+        'nullable'             | 'value'        | null
+        'nullable'             | 'beginDate'    | null
+        'nullable'             | 'createdAt'    | null
     }
 }
