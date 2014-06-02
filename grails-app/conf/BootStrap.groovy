60a3e306ef2a2d94a7f1132e489d9842f365f60b
André Santos <andreccdr@gmail.com>
2014-06-02 21:09:03 +0100

    Expense, User and Circle seeds

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,30 +1,92 @@
-import com.billmate.Circle
 import com.billmate.Collective
+import com.billmate.CustomExpenseType
+import com.billmate.DefaultExpenseType
+import com.billmate.ExpenseType
 import com.billmate.House
+import com.billmate.OccasionalExpense
 import com.billmate.User
 import com.billmate.RegisteredUser
 
 class BootStrap {
     def init = { servletContext ->
-        def user = new RegisteredUser(name: 'Bill Mates', email: 'bill@mate.com', password: 'billmate')
-        if(!user.secureSave()){
-            println(user.getErrors().getAllErrors().toString())
+
+        if(User.count() == 0){
+            new RegisteredUser(name: 'Bill Mates', email: 'bill@mate.com', password: 'billmate').secureSave()
+            new RegisteredUser(name: 'André Santos', email: 'andreccdr@gmail.com', password: 'asantos').secureSave()
+            new RegisteredUser(name: 'Pedro Leite', email: 'pmcleite@gmail.com', password: 'pleite').secureSave()
+            new RegisteredUser(name: 'Francisco Neves', email: 'fntneves@gmail.com', password: 'fneves').secureSave()
+            new RegisteredUser(name: 'Ricardo Branco', email: '28.ricardobranco@gmail.com', password: 'rbranco').secureSave()
         }
 
-        def house = new House(name: 'Casa de Braga')
-        if(!house.secureSave()){
-            println(house.getErrors().getAllErrors().toString())
+        if(House.count() == 0){
+            def house = new House(name: 'Casa BillMate')
+            house.secureSave()
+
+            User.findWhere(email: 'bill@mate.com').addToCircles(house.circle)
+            User.findWhere(email: 'andreccdr@gmail.com').addToCircles(house.circle)
+            User.findWhere(email: 'pmcleite@gmail.com').addToCircles(house.circle)
+            User.findWhere(email: 'fntneves@gmail.com').addToCircles(house.circle)
+            User.findWhere(email: '28.ricardobranco@gmail.com').addToCircles(house.circle)
         }
 
-        def collective = new Collective(name: 'Futeboladas CeSIUM')
-        if(!collective.secureSave()){
-            println(collective.getErrors().getAllErrors().toString())
+        if(Collective.count() == 0){
+            def collective = new Collective(name: 'Futeboladas CeSIUM')
+            collective.secureSave()
+
+            User.findWhere(email: 'bill@mate.com').addToCircles(collective.circle)
+            User.findWhere(email: 'andreccdr@gmail.com').addToCircles(collective.circle)
+            User.findWhere(email: 'pmcleite@gmail.com').addToCircles(collective.circle)
+            User.findWhere(email: 'fntneves@gmail.com').addToCircles(collective.circle)
+            User.findWhere(email: '28.ricardobranco@gmail.com').addToCircles(collective.circle)
         }
 
-        user.addToCircles(house.circle)
-        user.addToCircles(collective.circle)
-        if(!user.secureSave()){
-            println(user.getErrors().getAllErrors().toString())
+        if(DefaultExpenseType.count() == 0){
+            def shoppingType = new DefaultExpenseType(name: 'Shopping', cssClass: "fa fa-shopping-cart")
+            shoppingType.secureSave()
+
+            def mealType = new DefaultExpenseType(name: 'Meal', cssClass: "fa fa-cutlery")
+            mealType.secureSave()
+        }
+
+        if(CustomExpenseType.count() == 0){
+            def maidType = new CustomExpenseType(name: 'Maid')
+            maidType.secureSave()
+
+            def fieldRentalType = new CustomExpenseType(name: 'Field Rental')
+            fieldRentalType.secureSave()
+        }
+
+        if(OccasionalExpense.count == 0){
+            def friday_dinner = new OccasionalExpense(title: 'Friday Dinner', value: 25.25, expenseType: ExpenseType.findWhere(name: 'Meal'), circle: House.first().getCircle(), responsible: RegisteredUser.findWhere(user: User.findWhere(email: 'fntneves@gmail.com')))
+            friday_dinner.secureSave()
+
+            friday_dinner.addToAssignedUsers(User.findWhere(email: 'andreccdr@gmail.com'))
+            friday_dinner.addToAssignedUsers(User.findWhere(email: 'pmcleite@gmail.com'))
+            friday_dinner.addToAssignedUsers(User.findWhere(email: 'bill@mate.com'))
+            friday_dinner.addToAssignedUsers(User.findWhere(email: '28.ricardobranco@gmail.com'))
+
+            def shopping = new OccasionalExpense(title: 'Shopping', value: 31.33, expenseType: ExpenseType.findWhere(name: 'Shopping'), circle: House.first().getCircle(), responsible: RegisteredUser.findWhere(user: User.findWhere(email: 'pmcleite@gmail.com')))
+            shopping.secureSave()
+
+            shopping.addToAssignedUsers(User.findWhere(email: 'andreccdr@gmail.com'))
+            shopping.addToAssignedUsers(User.findWhere(email: 'fntneves@gmail.com'))
+            shopping.addToAssignedUsers(User.findWhere(email: '28.ricardobranco@gmail.com'))
+
+            def maid = new OccasionalExpense(title: 'Maid', value: 15.00, expenseType: ExpenseType.findWhere(name: 'Maid'), circle: House.first().getCircle(), responsible: RegisteredUser.findWhere(user: User.findWhere(email: 'pmcleite@gmail.com')))
+            maid.secureSave()
+
+            maid.addToAssignedUsers(User.findWhere(email: 'bill@mate.com'))
+            maid.addToAssignedUsers(User.findWhere(email: 'andreccdr@gmail.com'))
+            maid.addToAssignedUsers(User.findWhere(email: 'fntneves@gmail.com'))
+            maid.addToAssignedUsers(User.findWhere(email: '28.ricardobranco@gmail.com'))
+
+            def rental = new OccasionalExpense(title: 'Soccer Field Rental', value: 95.00, expenseType: ExpenseType.findWhere(name: 'Field Rental'), circle: Collective.first().getCircle(), responsible: RegisteredUser.findWhere(user: User.findWhere(email: '28.ricardobranco@gmail.com')))
+            rental.secureSave()
+
+            rental.addToAssignedUsers(User.findWhere(email: 'bill@mate.com'))
+            rental.addToAssignedUsers(User.findWhere(email: 'andreccdr@gmail.com'))
+            rental.addToAssignedUsers(User.findWhere(email: 'fntneves@gmail.com'))
+            rental.addToAssignedUsers(User.findWhere(email: 'pmcleite@gmail.com'))
         }
     }
 
