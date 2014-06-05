7ebd5bd47d08888c81fc60fa1993d77858c18cf6
pmcleite <pmcleite@gmail.com>
2014-06-05 15:04:34 +0100

    add ajax to notifications

---
+++ grails-app/assets/javascripts/bm-ajax-notifications.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".side-widget-content").click(function(){
+        var $notification = $(this);
+        $.ajax({
+            type: "GET",
+            url: '../notification/makeRead',
+            data: { id: 34 },
+            dataType: 'json'
+        }).done(function(response){
+            if(response.notification){
+                $notification.children(".user-details-wrapper").children(".user-details-count-wrapper").children(".status-icon").removeClass("red");
+                $notification.children(".user-details-wrapper").children(".user-details-count-wrapper").children(".status-icon").addClass("green");
+                var valueOfCountNotifications = parseInt($(".chat-message-count").text());
+                var valueDec = valueOfCountNotifications-1;
+                if(valueDec > 0) {
+                    $(".chat-message-count").text(valueDec);
+                }
+                else{
+                    $(".chat-message-count").remove();
+                }
+            }
+        });
+    });
+});
--- grails-app/controllers/com/billmate/BaseController.groovy
+++ grails-app/controllers/com/billmate/BaseController.groovy
@@ -1,10 +1,19 @@
 package com.billmate
 
+import grails.converters.JSON
+
 abstract class BaseController {
     private alreadyAuthenticated() {
         if(session.user) {
             redirect(uri: '/')
             return false
         }
+    }
+
+    public ajaxMakeReadNotification(){
+        int id = params.id
+        SystemNotification notification = SystemNotification.ajaxMakeReadNotification(id)
+        def response = ['notification': notification?.getIsRead()]
+        render response as JSON
     }
 }
